eb6b2de3b66cc0a3f0c767bb417de1f9f22b33e6
Ghufran Akbar <ghufran.akbar99@gmail.com>
2023-11-11 19:54:04 +0100

    updated setup file for pip installable

--- setup.py
+++ setup.py
@@ -1,15 +1,23 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+
+# setup(name='math_quiz',
+#         version='1.0',
+#         description='data science survival skills assignment 2',
+#       author='Muhammad Ghufran Akbar',
+#       packages=['math_quiz'],
+#       zip_safe = False)
+
+# setup(name='tests_math_quiz',
+#         version='1.0',
+#         description='data science survival skills assignment 2 tests',
+#       author='Muhammad Ghufran Akbar',
+#       packages=['tests_math_quiz'],
+#       zip_safe = False)
 
 setup(name='math_quiz',
         version='1.0',
         description='data science survival skills assignment 2',
       author='Muhammad Ghufran Akbar',
-      packages=['math_quiz'],
-      zip_safe = False)
-
-setup(name='tests_math_quiz',
-        version='1.0',
-        description='data science survival skills assignment 2 tests',
-      author='Muhammad Ghufran Akbar',
-      packages=['tests_math_quiz'],
-      zip_safe = False)
+      zip_safe = False,
+      packages=find_packages(include=['math_quiz','tests_math_quiz','math_quiz.*'])
+      )
