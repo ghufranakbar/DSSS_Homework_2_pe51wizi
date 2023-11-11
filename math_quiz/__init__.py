a20e4b9e7550972bc8259eabc7b8e8a0f8325306
Ghufran Akbar <ghufran.akbar99@gmail.com>
2023-11-11 20:33:30 +0100

    updated files for installable pip

--- math_quiz/__init__.py
+++ math_quiz/__init__.py
@@ -1,1 +1,2 @@
 from .math_quiz import math_quiz
+from .tests_math_quiz import TestMathGame
--- setup.py
+++ setup.py
@@ -14,10 +14,29 @@ from setuptools import setup, find_packages
 #       packages=['tests_math_quiz'],
 #       zip_safe = False)
 
-setup(name='math_quiz',
-        version='1.0',
-        description='data science survival skills assignment 2',
-      author='Muhammad Ghufran Akbar',
-      zip_safe = False,
-      packages=find_packages(include=['math_quiz','tests_math_quiz','math_quiz.*'])
-      )
+# setup(name='math_quiz',
+#         version='1.0',
+#         description='data science survival skills assignment 2',
+#       author='Muhammad Ghufran Akbar',
+#       zip_safe = False,
+#       packages=find_packages(include=['math_quiz','tests_math_quiz','math_quiz.*'])
+#       )
+
+from setuptools import setup, find_packages
+
+setup(
+    name='math_quiz',
+    version='1.0',
+    description='Data Science Survival Skills Assignment 2',
+    author='Muhammad Ghufran Akbar',
+    zip_safe=False,
+    packages=find_packages(include=['math_quiz', 'math_quiz.*']),
+    # install_requires=[
+    #     # List any dependencies your project might have
+    # ],
+    entry_points={
+        'console_scripts': [
+            'math_quiz = math_quiz.math_quiz:main',
+        ],
+    },
+)
